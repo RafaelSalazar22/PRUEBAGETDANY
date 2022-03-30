11df68f6be834454ca2cd7769aea51214b452327
RafaelSalazar22 <91817560+RafaelSalazar22@users.noreply.github.com>
2022-03-29 22:08:11 -0600

    Add files via upload

---
+++ Clubs/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\rafae\\3D Objects\\ClubsApp-main\\ClubsApp-main\\Clubs" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\rafae\\3D Objects\\ClubsApp-main\\ClubsApp-main\\Clubs"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\rafae\\3D Objects\\ClubsApp-main\\ClubsApp-main\\Clubs\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\rafae\\3D Objects\\ClubsApp-main\\ClubsApp-main\\Clubs"
+  "FLUTTER_TARGET=C:\\Users\\rafae\\3D Objects\\ClubsApp-main\\ClubsApp-main\\Clubs\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\rafae\\3D Objects\\ClubsApp-main\\ClubsApp-main\\Clubs\\.dart_tool\\package_config.json"
+)
